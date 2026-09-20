4983e2ffc0873a48cdd581dc2a38c7de08b22618
Ruchkin Aleksandr <sasharuchkin2006@vk.com>
2026-09-20 14:39:01 +0300

    Update SolverFactory.cs

--- SlaSolver/Solvers/SolverFactory.cs
+++ SlaSolver/Solvers/SolverFactory.cs
@@ -3,7 +3,8 @@
     {
         public static IReadOnlyList<ISolver> All { get; } = new List<ISolver>
         {
-            new SolverGauss(),
+            new SolverGauss(), // 3 метод
+            new SolverLUDecomposition(), // 6 метод
             //сюда будем добавлять все методы решения
             
         };
