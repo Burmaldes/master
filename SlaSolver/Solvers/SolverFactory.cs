8b5b4e49e66817e9015391036939dbd20c4938b8
Ruchkin Aleksandr <sasharuchkin2006@vk.com>
2026-09-20 14:23:55 +0300

    Update SolverFactory.cs

--- SlaSolver/Solvers/SolverFactory.cs
+++ SlaSolver/Solvers/SolverFactory.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SlaSolver.Solvers {
+﻿namespace SlaSolver.Solvers {
     public class SolverFactory 
     {
         public static IReadOnlyList<ISolver> All { get; } = new List<ISolver>
         {
-            new GaussSolver(),
+            new SolverGauss(),
             //сюда будем добавлять все методы решения
             
         };
