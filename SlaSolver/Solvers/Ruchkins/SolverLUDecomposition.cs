5cb96416c72eac6faddacb17c59ad7148dc2aa06
Aleksandr Ruchkin <sasharuchkin2006@vk.com>
2026-09-20 18:40:18 +0300

    Update SolverLUDecomposition.cs

--- SlaSolver/Solvers/Ruchkins/SolverLUDecomposition.cs
+++ SlaSolver/Solvers/Ruchkins/SolverLUDecomposition.cs
@@ -9,7 +9,70 @@ namespace SlaSolver.Solvers.Ruchkins
 
         public double[] Solve(double[,] matrixA, double[] vectorB)
         {
-            throw new NotImplementedException();
+            int n = matrixA.GetLength(0);
+
+            double[,] L = new double[n, n];
+            double[,] U = new double[n, n];
+
+            // LU-разложение: A = L * U
+            // Диагональ U равна 1
+            for (int k = 0; k < n; k++)
+            {
+                U[k, k] = 1;
+
+                // Вычисляем k-й столбец L
+                for (int i = k; i < n; i++)
+                {
+                    double sum = 0;
+
+                    for (int j = 0; j < k; j++)
+                        sum += L[i, j] * U[j, k];
+
+                    L[i, k] = matrixA[i, k] - sum;
+                }
+
+                if (Math.Abs(L[k, k]) < 1e-12)
+                    throw new Exception("LU-разложение невозможно.");
+
+                // Вычисляем k-ю строку U
+                for (int j = k + 1; j < n; j++)
+                {
+                    double sum = 0;
+
+                    for (int i = 0; i < k; i++)
+                        sum += L[k, i] * U[i, j];
+
+                    U[k, j] = (matrixA[k, j] - sum) / L[k, k];
+                }
+            }
+
+            // Решаем L * y = b
+            double[] y = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                double sum = 0;
+
+                for (int j = 0; j < i; j++)
+                    sum += L[i, j] * y[j];
+
+                y[i] = (vectorB[i] - sum) / L[i, i];
+            }
+
+            // Решаем U * x = y
+            double[] x = new double[n];
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                double sum = 0;
+
+                for (int j = i + 1; j < n; j++)
+                    sum += U[i, j] * x[j];
+
+                x[i] = y[i] - sum;
+            }
+
+            return x;
         }
     }
 }
